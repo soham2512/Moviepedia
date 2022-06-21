5c763a6c718cda26dcd8a131173d956f4e8afb8a
Soham Shah <31480832+soham2512@users.noreply.github.com>
2022-06-21 18:05:57 -0400

    Update info.py

--- Moviepedia/info.py
+++ Moviepedia/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'moviepediamail@gmail.com'
-EMAIL_HOST_PASSWORD = 'bktukiifqyimzize'
+EMAIL_HOST_PASSWORD = 'dotnrdurqataryuh'
 EMAIL_PORT = 587
